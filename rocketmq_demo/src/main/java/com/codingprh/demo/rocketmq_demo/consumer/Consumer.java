ec359e763b48f26a4f9f589b6db97b4356579ad4
zhouxiang <956952610@qq.com>
2019-08-22 11:44:14 +0800

    fix the message queue

--- rocketmq_demo/src/main/java/com/codingprh/demo/rocketmq_demo/consumer/Consumer.java
+++ rocketmq_demo/src/main/java/com/codingprh/demo/rocketmq_demo/consumer/Consumer.java
@@ -10,7 +10,9 @@ import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.common.message.MessageQueue;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -54,8 +56,49 @@ public class Consumer {
         Set<MessageQueue> mqs = consumer.fetchSubscribeMessageQueues("TopicTest");
         for (MessageQueue mq : mqs) {
             System.err.println("Consume from the queue: " + mq);
+            SINGLE_MQ:
+            while (true) {
+                try {
+                    PullResult pullResult = consumer.pullBlockIfNotFound(mq, null, getMessageQueueOffset(mq), 32);
+                    System.out.println(pullResult);
+                    putMessageQueueOffset(mq, pullResult.getNextBeginOffset());
 
+                    switch (pullResult.getPullStatus()) {
+                        case FOUND:
+                            List<MessageExt> messageExtList = pullResult.getMsgFoundList();
+                            for (MessageExt m : messageExtList) {
+                                System.out.println(new String(m.getBody()));
+                            }
+                            break;
+                        case NO_MATCHED_MSG:
+                            break;
+                        case NO_NEW_MSG:
+                            break SINGLE_MQ;
+                        case OFFSET_ILLEGAL:
+                            break;
+                        default:
+                            break;
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
+        consumer.shutdown();
+    }
+
+    private static final Map<MessageQueue, Long> offsetTable = new HashMap<MessageQueue, Long>();
+
+    private static void putMessageQueueOffset(MessageQueue mq, long offset) {
+        offsetTable.put(mq, offset);
+    }
+
+    private static long getMessageQueueOffset(MessageQueue mq) {
+        Long offset = offsetTable.get(mq);
+        if (offset != null)
+            return offset;
+        return 0;
     }
 
 }

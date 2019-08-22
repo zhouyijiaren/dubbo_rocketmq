65a95bb07044fca330094462da7e39f3beeae5f8
zhouxiang <956952610@qq.com>
2019-08-22 14:46:57 +0800

    print messageID

--- rocketmq_demo/src/main/java/com/codingprh/demo/rocketmq_demo/consumer/Consumer.java
+++ rocketmq_demo/src/main/java/com/codingprh/demo/rocketmq_demo/consumer/Consumer.java
@@ -68,7 +68,8 @@ public class Consumer {
                         case FOUND:
                             List<MessageExt> messageExtList = pullResult.getMsgFoundList();
                             for (MessageExt m : messageExtList) {
-                                System.out.println(mq.getQueueId()+"========="+count++ + "=======================" + new String(m.getBody()));
+                                System.out.println(m.getMsgId());
+                                ///System.out.println(mq.getQueueId()+"========="+count++ + "=======================" + new String(m.getBody()));
                             }
                             break;
                         case NO_MATCHED_MSG:
